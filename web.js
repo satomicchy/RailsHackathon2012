ef9af78e47de4debb153950087db410bcfb3bded
satomicchy <satomi_t_sora@yahoo.co.jp>
2012-06-04 20:31:04 +0900

    add listen port for production

--- web.js
+++ web.js
@@ -62,6 +62,8 @@ app.get('/schedule', function(req, res){
     });
 });
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
